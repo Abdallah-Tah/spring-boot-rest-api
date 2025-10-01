d53113d3b1c7a61a546429cd32d98cdb48be0d14
Abdallah-Tah <abdal_cascad@hotmail.com>
2025-10-01 09:30:03 -0400

    Add component scanning for the rest API package

--- src/main/java/com/amohamed/restapi/spring_boot_rest_api/SpringBootRestApiApplication.java
+++ src/main/java/com/amohamed/restapi/spring_boot_rest_api/SpringBootRestApiApplication.java
@@ -2,8 +2,10 @@ package com.amohamed.restapi.spring_boot_rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.amohamed.restapi" })
 public class SpringBootRestApiApplication {
 
 	public static void main(String[] args) {
